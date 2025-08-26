f73c26c2407f01922c0f6e7eea3578a57bffa210
dovvahkiin <bozidartovarnickii@gmail.com>
2025-08-26 15:36:38 +0200

    Created project for pre-exam in C language

---
+++ seminarski.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct bashKomande
+{
+    char tipKomande[5];
+    char pomocniTekst[255];
+} bash;
+
+main()
+{
+    struct bashKomande bash;
+    int izborTipa, generalniIzbor;
+    system("color 2");
+    printf("Hello World");
+}
